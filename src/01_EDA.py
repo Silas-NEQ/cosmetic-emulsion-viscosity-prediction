13ac796f48bb9581c0dcec416e35e5f4998b70e7
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-13 20:43:49 -0300

    build: add EDA script

---
+++ src/01_EDA.py
@@ -0,0 +1,10 @@
+# Exploratory Data Analysis
+
+# Libraries used
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Database
+pd.read_csv('data\\raw\\cosmetic_emulsion_data.csv')
