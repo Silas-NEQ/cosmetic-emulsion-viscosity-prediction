007215a0a545740ce7f3a9735a60ceb73b23c68d
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-15 14:42:52 -0300

    encoding and changes in print display

--- src/02_preprocessing.py
+++ src/02_preprocessing.py
@@ -5,6 +5,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 # Database
 df = pd.read_csv('data\\raw\\cosmetic_emulsion_data.csv')
@@ -16,4 +18,15 @@ x = df.iloc[:, 0:7].to_numpy()
 y = df.iloc[:, 7].to_numpy()
 
 # Shape
-print(x.shape, y.shape)
+print(f'X Shape: {x.shape} | Y Shape: {y.shape}')
+
+# Encoding
+# Label encoder
+encoder = LabelEncoder()
+x[:,6] = encoder.fit_transform(x[:,6])
+# OneHot encoder
+onehotencoder = ColumnTransformer(
+    transformers=[('OneHot', OneHotEncoder(sparse_output=False), [6])], 
+    remainder='passthrough')
+x = onehotencoder.fit_transform(x)
+print(f'New X shape: {x.shape}')
