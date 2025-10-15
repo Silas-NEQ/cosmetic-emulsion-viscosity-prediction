64d4f4956fa0c7a9ae48147198d612e1ac44e05a
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-15 17:50:56 -0300

    load processed data

---
+++ src/03_modeling.py
@@ -0,0 +1,19 @@
+# Data Modeling
+
+# Libraries used
+import pickle
+import time
+from sklearn.model_selection import GridSearchCV
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Database
+with open('data\\processed\\processed_data.pkl', mode='rb') as f:
+    x_test, x_train, y_test, y_train = pickle.load(f)
+print(f'Shape X Train: {x_train.shape} | Shape X Test {x_test.shape}')
+print(f'Shape Y Train: {y_train.shape} | Shape Y Test {y_test.shape}')
