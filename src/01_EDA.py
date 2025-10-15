9680bbefd47ae810bd7dcbb1bd0201c471d17983
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-15 10:44:28 -0300

    add the script 01_EDA.py in src folder

--- src/01_EDA.py
+++ src/01_EDA.py
@@ -7,4 +7,44 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Database
-pd.read_csv('data\\raw\\cosmetic_emulsion_data.csv')
+df = pd.read_csv('data\\raw\\cosmetic_emulsion_data.csv')
+
+# Head
+print(df.head())
+
+# Shape
+print(df.shape)
+
+# Info
+print(df.info())
+
+# Columns
+print(df.columns)
+
+# Null values 
+# No null values
+print(df.isnull().sum())
+
+# Numerical predictors
+numerical_pred = df.select_dtypes(include=['number']).columns.drop('Viscosidade_final_cP')
+
+
+# Scater graphs
+for col in numerical_pred:
+    sns.scatterplot(df, y='Viscosidade_final_cP', x=col)
+    plt.title(f'{col} x Viscosidade_final_cP')
+    #plt.show()
+    #input('Press Enter to continue...')
+
+# Boxplots
+for col in numerical_pred:
+    sns.boxplot(df, y=col)
+    plt.title(f"{col} Distribution")
+    #plt.show()
+    #input('Press Enter to continue...')
+
+# Correlation
+corr = df.select_dtypes(include=['number']).corr()
+sns.heatmap(corr, annot=True, cmap='coolwarm', fmt='.2f')
+plt.title('Correlation Map')
+plt.show()
