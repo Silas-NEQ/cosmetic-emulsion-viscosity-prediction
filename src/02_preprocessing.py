31856e6297d54bd64a767e29a60bc73711d1dcaf
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-15 11:44:18 -0300

    creat the script 02_preprocessing.py in src folder

---
+++ src/02_preprocessing.py
@@ -0,0 +1,10 @@
+# Data preprocessing
+
+# Library used
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+# Database
+df = pd.read_csv('data\\raw\\cosmetic_emulsion_data.csv')
