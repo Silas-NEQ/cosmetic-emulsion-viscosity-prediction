b76dcf7e877d67b28008f11cd3993ac483bf6b4f
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-21 17:37:03 -0300

    create the script 05_dimensionality_analyse.py in src folder

---
+++ src/05_dimensionality_analyse.py
@@ -0,0 +1,10 @@
+# Dimensionality Analyse
+
+# Libraries used
+import pickle
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
