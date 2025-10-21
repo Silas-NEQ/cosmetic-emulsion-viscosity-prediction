357e11f7f03606a56cad0c053ea3647da80c5b03
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-21 18:08:11 -0300

    add models and import pipeline

--- src/05_dimensionality_analyse.py
+++ src/05_dimensionality_analyse.py
@@ -5,6 +5,7 @@ import pickle
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.neural_network import MLPRegressor
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
@@ -21,3 +22,13 @@ pca = PCA(n_components=0.95)
 x_train_pca = pca.fit_transform(x_train)
 # New x_test
 x_test_pca = pca.transform(x_test)
+
+# Models
+# Neural Network
+nn_model = MLPRegressor(activation='relu', alpha=0.01, hidden_layer_sizes= (50,),
+                        learning_rate_init=0.001, solver='lbfgs', max_iter=2000)
+# Polymial Regression
+poly_model = Pipeline(
+    [('poly', PolynomialFeatures(degree=2)),('regressor', LinearRegression())])
+# XGBoost
+xgboost_model = XGBRegressor(n_estimators=300, max_depth=3, learning_rate=0.05)
