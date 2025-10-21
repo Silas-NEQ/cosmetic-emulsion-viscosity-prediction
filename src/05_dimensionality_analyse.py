00dfbe6d313de0ea67385017ebf67ca2ab0af12d
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-21 18:00:09 -0300

    pca anlyse

--- src/05_dimensionality_analyse.py
+++ src/05_dimensionality_analyse.py
@@ -14,3 +14,10 @@ with open('data\\processed\\processed_data.pkl', mode='rb') as f:
     x_test, x_train, y_test, y_train = pickle.load(f)
 print(f'Shape X Train: {x_train.shape} | Shape X Test {x_test.shape}')
 print(f'Shape Y Train: {y_train.shape} | Shape Y Test {y_test.shape}')
+
+# PCA
+pca = PCA(n_components=0.95)
+# New x_train
+x_train_pca = pca.fit_transform(x_train)
+# New x_test
+x_test_pca = pca.transform(x_test)
