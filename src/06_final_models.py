4150018651950b384231ffb35eac06658c1ecfa4
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-22 15:40:50 -0300

    import libraries to 06_final_models.py script

---
+++ src/06_final_models.py
@@ -0,0 +1,12 @@
+# Final models Set
+
+# Libraries used
+import pickle
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.neural_network import MLPRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from xgboost import XGBRegressor
+
