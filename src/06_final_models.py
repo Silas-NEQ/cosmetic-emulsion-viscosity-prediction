b1be023c6c9f54039ab5d3892c3ecf8fcef70aa2
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-22 16:15:22 -0300

    concatenate x and y

--- src/06_final_models.py
+++ src/06_final_models.py
@@ -15,3 +15,7 @@ with open('data\\processed\\processed_data.pkl', mode='rb') as f:
     x_test, x_train, y_test, y_train = pickle.load(f)
 print(f'Shape X Train: {x_train.shape} | Shape X Test {x_test.shape}')
 print(f'Shape Y Train: {y_train.shape} | Shape Y Test {y_test.shape}')
+# X
+x = pd.concat([x_train, x_test], axis=0)
+# Y
+y = pd.concat([y_train, y_test], axis=0)
