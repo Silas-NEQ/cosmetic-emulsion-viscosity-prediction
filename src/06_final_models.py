43f0ecbbbd6d883c9103000049957654b3171989
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-22 15:41:59 -0300

    import database to 06_final_models.py script

--- src/06_final_models.py
+++ src/06_final_models.py
@@ -10,3 +10,8 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
 
+# Database
+with open('data\\processed\\processed_data.pkl', mode='rb') as f:
+    x_test, x_train, y_test, y_train = pickle.load(f)
+print(f'Shape X Train: {x_train.shape} | Shape X Test {x_test.shape}')
+print(f'Shape Y Train: {y_train.shape} | Shape Y Test {y_test.shape}')
