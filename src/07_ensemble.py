dec4f04860dd498197c77a3ce53edba4f7f0be93
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-23 18:40:21 -0300

    import database to 07_ensemble.py script

--- src/07_ensemble.py
+++ src/07_ensemble.py
@@ -3,3 +3,8 @@
 # Library used
 import pickle
 
+# Database
+with open('data\\processed\\processed_data.pkl', mode='rb') as f:
+    x_test, x_train, y_test, y_train = pickle.load(f)
+print(f'Shape X Train: {x_train.shape} | Shape X Test {x_test.shape}')
+print(f'Shape Y Train: {y_train.shape} | Shape Y Test {y_test.shape}')
