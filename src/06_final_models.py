9454db06ae2e0a1c2749bc72ec084d8d345bb7ba
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-23 15:19:35 -0300

    remove unused imports

--- src/06_final_models.py
+++ src/06_final_models.py
@@ -1,9 +1,8 @@
-# Final models Set
+# Final Models Set
 
 # Libraries used
 import pickle
 import pandas as pd
-from sklearn.decomposition import PCA
 from sklearn.neural_network import MLPRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
