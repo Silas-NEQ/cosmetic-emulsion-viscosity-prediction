ee6b3ca9ff7084067dc661ae0ecffd0d8755d633
Silas Nascimento <silas.nascimento2513@gmail.com>
2025-10-23 18:44:04 -0300

    import models to 07_ensemble.py script

--- src/07_ensemble.py
+++ src/07_ensemble.py
@@ -8,3 +8,11 @@ with open('data\\processed\\processed_data.pkl', mode='rb') as f:
     x_test, x_train, y_test, y_train = pickle.load(f)
 print(f'Shape X Train: {x_train.shape} | Shape X Test {x_test.shape}')
 print(f'Shape Y Train: {y_train.shape} | Shape Y Test {y_test.shape}')
+
+# Models
+# Neural Network
+nn_model = pickle.load(open('models\\neural_network_model.pkl', 'rb'))
+# Polymial Regression
+poly_model = pickle.load(open('models\\polynomial_regression.pkl', 'rb'))
+# XGBoost
+xgb_model = pickle.load(open('models\\xgboost_model.pkl', 'rb'))
